2392e68777221d4a48fa1d2d3553fd77ccd0af7f
lihon <gelf.learing@gmail.com>
2024-07-13 10:39:21 +0300

    1-power the power of a function using reccuritons

---
+++ recursion/1-power.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" a function used to calculate the power of
+    number using recurtion
+"""
+
+
+def power(x: int, y: int) -> float:
+    """
+        finding x**5 using rectution
+    """
+    if y == 1:
+        return x
+    return x * power(x, y-1)
+print(power(5, 3))
