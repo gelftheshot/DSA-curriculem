7ab5683f20cebaa9e40cb36c6d95b174d5229037
lihon <gelf.learing@gmail.com>
2024-07-13 10:40:16 +0300

    decimal to binary using reccurtions

---
+++ recursion/3-decimal-to-binary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    decimal to binary using a recution
+"""
+def decimal_to_binary(n: int) -> str:
+    """
+        a function to change the decimal to binary
+    """
+    if n == 0 or n == 1:
+        return str(n)
+    return decimal_to_binary(n // 2) + str(n % 2)
+print(decimal_to_binary(8))
